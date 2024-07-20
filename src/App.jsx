230a7162d44db3842108bfbf71955adcfb7a4e74
Raktim Shrestha <rakimsth@gmail.com>
2024-07-20 12:44:44 +0545

    added footer design

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import Footer from "./layouts/Footer";
+import Home from "./pages/Home";
+
 function App() {
-  return <>Hello world</>;
+  return (
+    <>
+      <div className="container vh-90">
+        <div
+          className="d-flex justify-content-center"
+          style={{ minHeight: "400px" }}
+        >
+          <Home />
+        </div>
+        <Footer />
+      </div>
+    </>
+  );
 }
 
 export default App;
