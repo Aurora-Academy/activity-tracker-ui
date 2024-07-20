279d67a5fb9a74076fccecf78beb7aeaf4253d73
Raktim Shrestha <rakimsth@gmail.com>
2024-07-20 13:08:31 +0545

    created input component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import { Card } from "react-bootstrap";
+import Input from "../components/Input";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="container">
+      <div className="row mt-2">
+        <h1 className="text-center">Activity Tracker</h1>
+      </div>
+      <div className="row">
+        <Card className="shadow p-3 mb-5 bg-white rounded">
+          <Card.Body>
+            <Input />
+          </Card.Body>
+        </Card>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
